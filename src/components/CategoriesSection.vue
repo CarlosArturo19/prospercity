05276e466dd86d70af55d7350c7525d349d1aac1
LUIS ALEJANDRO GOMEZ CASTANO <LAGOMCAS@davivienda.loc>
2022-08-26 17:32:12 -0500

    Categorias

--- src/components/CategoriesSection.vue
+++ src/components/CategoriesSection.vue
@@ -2,40 +2,40 @@
     <section class="ftco-section ftco-no-pb">
         <div class="container">
             <div class="row">
-                <div class="col-lg-2 col-md-4 ">
+                <div class="col-lg-2 col-md-4 cursor-pointer">
                     <div class="sort w-100 text-center ftco-animate fadeInUp ftco-animated">
-                        <div class="img img-category"></div>
-                        <h3>Brandy</h3>
+                        <div class="img img-category1"></div>
+                        <h3>Cerveza</h3>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-4 ">
+                <div class="col-lg-2 col-md-4 cursor-pointer">
                     <div class="sort w-100 text-center ftco-animate fadeInUp ftco-animated">
-                        <div class="img img-category"></div>
-                        <h3>Gin</h3>
+                        <div class="img img-category2"></div>
+                        <h3>Aguardiente</h3>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-4 ">
+                <div class="col-lg-2 col-md-4 cursor-pointer">
                     <div class="sort w-100 text-center ftco-animate fadeInUp ftco-animated">
-                        <div class="img img-category"></div>
-                        <h3>Rum</h3>
+                        <div class="img img-category3"></div>
+                        <h3>Ron</h3>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-4 ">
+                <div class="col-lg-2 col-md-4 cursor-pointer">
                     <div class="sort w-100 text-center ftco-animate fadeInUp ftco-animated">
-                        <div class="img img-category"></div>
-                        <h3>Tequila</h3>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-4 ">
-                    <div class="sort w-100 text-center ftco-animate fadeInUp ftco-animated">
-                        <div class="img img-category"></div>
+                        <div class="img img-category4"></div>
                         <h3>Vodka</h3>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-4 ">
+                <div class="col-lg-2 col-md-4 cursor-pointer">
                     <div class="sort w-100 text-center ftco-animate fadeInUp ftco-animated">
-                        <div class="img img-category"></div>
+                        <div class="img img-category5"></div>
                         <h3>Whiskey</h3>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 cursor-pointer">
+                    <div class="sort w-100 text-center ftco-animate fadeInUp ftco-animated">
+                        <div class="img img-category6"></div>
+                        <h3>Más...</h3>
                     </div>
                 </div>
 
@@ -53,9 +53,39 @@ export default {
 </script>
 
 <style>
-    .img-category
+    .img-category1
     {
-        background-image: url(../assets/images/kind-1.jpg);
+        background-image: url(../assets/images/categories/kind-1.jpg);
+    }
+
+    .img-category2
+    {
+        background-image: url(../assets/images/categories/kind-2.jpg);
+    }
+
+    .img-category3
+    {
+        background-image: url(../assets/images/categories/kind-3.jpg);
+    }
+
+    .img-category4
+    {
+        background-image: url(../assets/images/categories/kind-4.jpg);
+    }
+
+    .img-category5
+    {
+        background-image: url(../assets/images/categories/kind-5.jpg);
+    }
+
+    .img-category6
+    {
+        background-image: url(../assets/images/categories/kind-6.jpg);
+    }
+
+    .cursor-pointer
+    {
+        cursor: pointer;
     }
 </style>
 
