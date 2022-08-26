58607445cf0904c19ab22c6125b84510296ce474
CarlosArturo19 <valenciahincapie9@gmail.com>
2022-08-26 16:58:09 -0500

    para colocarle el titulo

--- src/components/InformationSection.vue
+++ src/components/InformationSection.vue
@@ -9,33 +9,30 @@
                             <span class="fa fa-phone"></span>
                         </div>
                         <div class="text">
-                            <h2>Online Support 24/7</h2>
-                            <p>A small river named Duden flows by their place and supplies it with the necessary
-                                regelialia.</p>
+                            <h2>soporte online</h2>
+                            <p>Servicio al cliente 24/7.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4 d-flex">
-                    <div class="intro color-1 d-lg-flex w-100 ftco-animate fadeInUp ftco-animated">
+                    <div class="intro color-1 d-lg-flex w-100 ftco-animate fadeInUp ftco-animated color-yellow">
                         <div class="icon">
-                            <span class="fa fa-phone"></span>
+                            <span class="fa fa-money"></span>
                         </div>
                         <div class="text">
-                            <h2>Money Back Guarantee</h2>
-                            <p>A small river named Duden flows by their place and supplies it with the necessary
-                                regelialia.</p>
+                            <h2>precios bajos</h2>
+                            <p>Todo a precio de estanquillo!.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4 d-flex">
                     <div class="intro color-2 d-lg-flex w-100 ftco-animate fadeInUp ftco-animated">
                         <div class="icon">
-                            <span class="fa fa-phone"></span>
+                            <span class="fa fa-truck"></span>
                         </div>
                         <div class="text">
-                            <h2>Free Shipping &amp; Return</h2>
-                            <p>A small river named Duden flows by their place and supplies it with the necessary
-                                regelialia.</p>
+                            <h2>domicilios </h2>
+                            <p>Domicilios gratis a partir de $150.000.</p>
                         </div>
                     </div>
                 </div>
@@ -52,5 +49,10 @@ export default {
 }
 </script>
 
-<style >
+<style>
+    .color-yellow
+    {
+        background-color: rgb(203, 193, 42) !important;
+    }
+
 </style>
