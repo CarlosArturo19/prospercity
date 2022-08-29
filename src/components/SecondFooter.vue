024d14565ad1c5ad9b045d0c9697cb3b55401dd2
CarlosArturo19 <valenciahincapie9@gmail.com>
2022-08-29 16:13:37 -0500

    cambios del fisrtfooter

--- src/components/SecondFooter.vue
+++ src/components/SecondFooter.vue
@@ -6,9 +6,9 @@
 
                     <p class="mb-0" style="color: rgba(255,255,255,.5);">
                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        Copyright &copy;{{ year }} Todos Los Derechos Reservados | Desarrolado por ...<i
-                            class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com"
-                            target="_blank">Colorlib.com</a>
+                        Copyright &copy;{{ year }} Todos Los Derechos Reservados | Desarrolado Por Carlos Arturo<i
+                            class="fa fa-heart color-danger" aria-hidden="true"></i><a href="https://colorlib.com"
+                            target="_blank"></a>
                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     </p>
                 </div>
